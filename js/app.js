b08de5d8ed5d91f8bca738e7463458bb34534a77
Himanshu Chauhan <hchauhan1404@outlook.com>
2015-12-18 10:07:13 +0530

    Yes! I made that effect working. B-)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#content").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#content').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
