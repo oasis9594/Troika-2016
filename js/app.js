6c226d914ea59c6c73c1e55ee3195125eaa0fdc3
sakshamsaxena <ontenth@live.com>
2015-12-21 14:10:06 +0530

    home page final

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
 	var height = movementStrength / $(window).height();
 	var width = movementStrength / $(window).width();
 	$("#content").mousemove(function (e) {
-		var pageX = e.pageX - ($(window).width() / 2);
-		var pageY = e.pageY - ($(window).height() / 2);
-		var newvalueX = width * pageX * -1 - 25;
-		var newvalueY = height * pageY * -1 - 50;
-		$('#content').css("background-position", newvalueX + "px     " + newvalueY + "px");
+		if($(window).width() > 768){
+			var pageX = e.pageX - ($(window).width() / 2);
+			var pageY = e.pageY - ($(window).height() / 2);
+			var newvalueX = width * pageX * -1 - 25;
+			var newvalueY = height * pageY * -1 - 50;
+			$('#content').css("background-position", newvalueX + "px     " + newvalueY + "px");
+		}
 	});
 	$('.slider').jcider({
 		"autoplay":true,
