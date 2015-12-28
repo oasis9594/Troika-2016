3e4c2bac225c84ec5b111d18111b943beb8f4c50
sakshamsaxena <ontenth@live.com>
2015-12-28 18:23:30 +0530

    Removed background for now

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-	var movementStrength = 25;
-	var height = movementStrength / $(window).height();
-	var width = movementStrength / $(window).width();
-	$("#content").mousemove(function (e) {
-		if($(window).width() > 768){
-			var pageX = e.pageX - ($(window).width() / 2);
-			var pageY = e.pageY - ($(window).height() / 2);
-			var newvalueX = width * pageX * -1 - 25;
-			var newvalueY = height * pageY * -1 - 50;
-			$('#content').css("background-position", newvalueX + "px     " + newvalueY + "px");
-		}
-	});
+//	var movementStrength = 25;
+//	var height = movementStrength / $(window).height();
+//	var width = movementStrength / $(window).width();
+//	$("#content").mousemove(function (e) {
+//		if($(window).width() > 768){
+//			var pageX = e.pageX - ($(window).width() / 2);
+//			var pageY = e.pageY - ($(window).height() / 2);
+//			var newvalueX = width * pageX * -1 - 25;
+//			var newvalueY = height * pageY * -1 - 50;
+//			$('#content').css("background-position", newvalueX + "px     " + newvalueY + "px");
+//		}
+//	});
 	$('.slider').jcider({
 		"autoplay":true,
 		"looping":true
